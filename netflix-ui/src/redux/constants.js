67254a200528f8a93ffd731e38661452c261fd56
le-dat <lqdat87@gmail.com>
2022-09-18 10:57:53 +0700

    deloy heroku api 1

--- netflix-ui/src/redux/constants.js
+++ netflix-ui/src/redux/constants.js
@@ -1,6 +1,4 @@
 export const API_URL =
-  process.env.NODE_ENV !== "production"
-    ? "https://intense-spire-08624.herokuapp.com"
-    : "https://intense-spire-08624.herokuapp.com";
+  process.env.NODE_ENV !== "production" ? "http://localhost:5000" : "https://ledat-mern-netflix.herokuapp.com";
 export const API_KEY = "3bb51c074c2acc0fd9437965ff84950f";
 export const TMDB_BASE_URL = "https://api.themoviedb.org/3";
